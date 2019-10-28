e45c3f4c3d224a0dfc947041eaf486f802207be6
IlSanna <IlSanna@users.noreply.github.com>
2019-10-28 17:21:17 +0100

    started

--- SDL/src/main.cpp
+++ SDL/src/main.cpp
@@ -1,23 +1,41 @@
 #include <SDL.h>
 #include <iostream>
+#include "main.h"
+
+SDL_Window* _window;
+SDL_Renderer* _renderer;
 
 const int SCREEN_X = 640;
 const int SCREEN_Y = 480;
+
+bool init(int screenX, int screenY) {
+	SDL_CreateWindowAndRenderer(SCREEN_X, SCREEN_Y, 0, &_window, &_renderer);
+	SDL_SetWindowTitle(_window, "SDL Works!");
+	return _window ? true : false;
+}
+
+void render() {
+	SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
+	SDL_RenderClear(_renderer);
+	SDL_RenderPresent(_renderer);
+}
 
 int main(int argc, char* argv[]) {
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0 ) {
 		std::cout << "failed to load SDL" << std::endl;
 	}
-	SDL_Window* _window;
-	SDL_Renderer* _renderer;
 
-	SDL_CreateWindowAndRenderer(SCREEN_X, SCREEN_Y,0, &_window, &_renderer);
+	bool running = false;
+
+	if (init(SCREEN_X, SCREEN_Y)) {
+		running = true;
+	}
+
+	while (running) {
+		render();
+	}
 	
-	SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
-	SDL_RenderClear(_renderer);
-	SDL_RenderPresent(_renderer);
-	SDL_Delay(5000);
 	SDL_Quit();
 
 	return 0;
